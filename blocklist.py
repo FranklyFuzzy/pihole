4bbfc2c2dc0c83ae413ff8c6daa81dc9953d1828
FranklyFuzzy <30601526+FranklyFuzzy@users.noreply.github.com>
2023-08-15 11:53:09 -0400

    Update blocklist.py

--- blocklist.py
+++ blocklist.py
@@ -5,9 +5,11 @@ url = 'https://firebog.net/'
 response = requests.get(url)
 soup=BeautifulSoup(response.text, 'html.parser')
 
+urls = []
+
 for li in soup.find_all('li', {'class': 'bdTick'}):
     if 'http' in li.text:
         #print(li.text.split(':', 1)[1])
-        output1 = (li.text.split(':', 1)[1])
-        output = output1.replace("/n"," ")
-        print(output, sep=' ')
+        urls.append(li.text.split(':', 1)[1])
+        
+print(*urls)
